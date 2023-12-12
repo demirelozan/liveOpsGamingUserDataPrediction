bc8b95c8d2c44dcfc61702f1dbb283259859aa23
OZAN\ozand <ozan.demirel@hotmail.com>
2023-12-12 17:42:00 +0300

    Added several methods and plots to visualize the data to analyze their effect on revenue

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ from ISLP import load_data
 fileLocation = "C:\\Users\\ozand\\Documents\\Python Projects\\liveOpsGamingUserDataPrediction\\ml_project_2023_cltv_train.xlsx"
 
 # Load the data
-data = pd.read_excel(fileLocation, engine = 'openpyxl')
+data = pd.read_excel(fileLocation, engine='openpyxl')
 
-# We'll create visualizations for a few parameters against revenue. We'll make use of scatter plots for continuous variables
-# and bar plots for categorical variables to see how they compare with revenue.
-
-# Define the continuous and categorical variables we want to compare with revenue.
-# Note: The actual variables will depend on your dataset. Please replace the placeholders with your real column names.
+# Continuous Variables are used create scatter plots against revenue.
+# Categorical Variables are used to create bar plots to see how they compare with revenue.
 continuous_vars = ['session_cnt', 'gameplay_duration', 'max_lvl_no']  # Example continuous variables
 categorical_vars = ['os', 'country', 'device_brand']  # Example categorical variables
 
@@ -26,14 +23,28 @@ for var in continuous_vars:
     plt.xlabel(var)
     plt.show()
 
+
+# Function to truncate labels
+def truncate_labels(labels, max_length):
+    """Truncate labels to a maximum character length."""
+    return [str(label)[:max_length] + '...' if len(str(label)) > max_length else str(label) for label in labels]
+
+
 # Bar plots for categorical variables
 for var in categorical_vars:
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(30, 6))
     sns.barplot(data=data, x=var, y='revenue', errorbar=None)
     plt.title(f'Average Revenue by {var}')
     plt.ylabel('Average Revenue')
     plt.xlabel(var)
-    plt.xticks(rotation=45)
+
+    # Apply truncation only for the Device Brands (to make it more readable).
+    if var == 'device_brand':
+        truncated_labels = truncate_labels(data[var].astype(str).unique(), 10)
+        plt.xticks(ticks=range(len(truncated_labels)), labels=truncated_labels, rotation=90, fontsize=6)
+    else:
+        plt.xticks(rotation=45, fontsize=8)
+
     plt.show()
 
 # Heatmap to show correlations of all variables with revenue
