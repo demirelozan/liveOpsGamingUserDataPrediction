45df4f65c2573f8f85a40cd88c3a30fc2a081289
OZAN\ozand <ozan.demirel@hotmail.com>
2023-12-13 16:02:50 +0300

    Refactoring, Added Scatterplot Matrix to see relations of parameters with each other, Added KMeans Clustering

--- dataVisualization.py
+++ dataVisualization.py
@@ -2,7 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from ISLP import load_data
+from sklearn.cluster import KMeans
+
 
 fileLocation = "C:\\Users\\ozand\\Documents\\Python Projects\\liveOpsGamingUserDataPrediction\\ml_project_2023_cltv_train.xlsx"
 
@@ -11,7 +12,7 @@ data = pd.read_excel(fileLocation, engine='openpyxl')
 
 # Continuous Variables are used create scatter plots against revenue.
 # Categorical Variables are used to create bar plots to see how they compare with revenue.
-continuous_vars = ['session_cnt', 'gameplay_duration', 'max_lvl_no']  # Example continuous variables
+continuous_vars = ['session_cnt', 'gameplay_duration', 'max_lvl_no', 'banner_cnt', 'is_cnt', 'rv_cnt']
 categorical_vars = ['os', 'country', 'device_brand']  # Example categorical variables
 
 # Scatter plots for continuous variables
@@ -38,7 +39,7 @@ for var in categorical_vars:
     plt.ylabel('Average Revenue')
     plt.xlabel(var)
 
-    # Apply truncation only for the Device Brands (to make it more readable).
+    # Applying truncation only for the Device Brands (to make it more readable), some brand names are too long.
     if var == 'device_brand':
         truncated_labels = truncate_labels(data[var].astype(str).unique(), 10)
         plt.xticks(ticks=range(len(truncated_labels)), labels=truncated_labels, rotation=90, fontsize=6)
@@ -49,7 +50,6 @@ for var in categorical_vars:
 
 # Heatmap to show correlations of all variables with revenue
 plt.figure(figsize=(12, 8))
-# Select only numeric columns for the correlation matrix
 numeric_data = data.select_dtypes(include=[np.number])
 correlation_matrix = numeric_data.corr()
 
@@ -57,3 +57,25 @@ sns.heatmap(correlation_matrix[['revenue']].sort_values(by='revenue', ascending=
             annot=True, cmap='coolwarm', center=0)
 plt.title('Correlation of Features with Revenue')
 print(plt.show())
+
+
+features = data[['session_cnt', 'gameplay_duration']]
+
+# KMeans Clustering
+kmeans = KMeans(n_clusters=3, random_state=0).fit(features)
+data['cluster'] = kmeans.labels_
+
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=data, x='session_cnt', y='gameplay_duration', hue='cluster', palette='viridis')
+plt.title('User Segmentation based on Session Count and Gameplay Duration')
+plt.show()
+
+# Creating a Scatterplot Matrix to see the relations of other parameters comparing to each other
+selected_columns = ['banner_cnt', 'session_cnt', 'session_length', 'max_lvl_no', 'gameplay_duration',
+                    'bonus_cnt', 'repeat_cnt', 'gold_cnt', 'is_cnt', 'rv_cnt', 'max_ses_length', 'avg_ses_length']
+
+data_subset = data[selected_columns]
+
+sns.pairplot(data_subset)
+plt.show()
