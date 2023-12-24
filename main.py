40d4d4eaf246f7aa0bd291e58efb8f1a7889ca22
OZAN\ozand <ozan.demirel@hotmail.com>
2023-12-24 16:06:16 +0300

    Retraining ML models with feature selection based on performances, changes in code ordering

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from featureEngineering import FeatureEngineering
+from modelTraining import ModelTraining
 import pandas as pd
 from plotting import generate_plots
 
@@ -11,7 +12,7 @@ def main():
     # Create a copy of the data for feature engineering
     data_with_features = original_data.copy()
 
-    generate_plots(original_data, include_new_features=False)
+    #    generate_plots(original_data, include_new_features=False)
 
     # Weight values are gathered from Correlation to Revenue
     weights = {'banner_cnt': 0.75, 'is_cnt': 0.61, 'rv_cnt': 0.69}
@@ -21,7 +22,56 @@ def main():
     data_with_features = feature_engineer.execute_all(weights)  # Add weights as needed
 
     # Generate plots for data with new features
-    generate_plots(data_with_features, include_new_features=True)
+    #    generate_plots(data_with_features, include_new_features=True)
+
+    print(list(data_with_features.columns))
+
+    model_trainer = ModelTraining(data_with_features)
+    model_trainer.preprocess_data()
+    #    model_trainer.split_data()
+
+    # Train various models
+    lr_model = model_trainer.train_linear_regression()
+    dt_model = model_trainer.train_decision_tree()
+    rf_model = model_trainer.train_random_forest()
+    xgb_model = model_trainer.train_xgboost()
+
+    lr_metrics = model_trainer.evaluate_model(lr_model)
+
+    dt_feature_importance = model_trainer.get_feature_importance(dt_model)
+    print("Decision Tree Feature Importance:\n", dt_feature_importance)
+    rf_feature_importance = model_trainer.get_feature_importance(rf_model)
+    print("Random Forest Feature Importance:\n", rf_feature_importance)
+    xgb_feature_importance = model_trainer.get_feature_importance(xgb_model)
+    print("Xgboost Feature Importance:\n", xgb_feature_importance)
+
+    # Evaluate models
+    print("Linear Regression Metrics:", lr_metrics)
+    print("Decision Tree Accuracy:", model_trainer.evaluate_model(dt_model))
+    print("Random Forest Accuracy:", model_trainer.evaluate_model(rf_model))
+    print("XGBoost Accuracy:", model_trainer.evaluate_model(xgb_model))
+
+    # Feature selection based on model importance
+    _, _, selected_features = model_trainer.feature_selection_based_on_importance(xgb_model)
+    print("Selected Features:", selected_features)
+
+    model_trainer.X_train, model_trainer.X_test, _ = model_trainer.feature_selection_based_on_importance(xgb_model)
+
+    lr_model_selected = model_trainer.train_linear_regression()
+    dt_model_selected = model_trainer.train_decision_tree()
+    rf_model_selected = model_trainer.train_random_forest()
+    xgb_model_selected = model_trainer.train_xgboost()
+
+    # Evaluate the model with selected features
+    lr_metrics_selected = model_trainer.evaluate_model(lr_model_selected)
+    dt_metrics_selected = model_trainer.evaluate_model(dt_model_selected)
+    rf_metrics_selected = model_trainer.evaluate_model(rf_model_selected)
+    xgb_metrics_selected = model_trainer.evaluate_model(xgb_model_selected)
+
+    print("Linear Regression with Selected Feature Metrics: ", lr_metrics_selected)
+    print("Decision Tree with Selected Feature Metrics: ", dt_metrics_selected)
+    print("Random Forest with Selected Feature Metrics: ", rf_metrics_selected)
+    print("XGBoost with Selected Feature Metrics: ", xgb_metrics_selected)
 
 
 if __name__ == "__main__":
--- modelTraining.py
+++ modelTraining.py
@@ -1,26 +1,97 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
 
 
 class ModelTraining:
     def __init__(self, data):
+        self.preprocessor = None
         self.data = data
         self.X_train, self.X_test, self.y_train, self.y_test = None, None, None, None
 
-    def split_data(self, test_size=0.20, random_state=42):
+    def preprocess_data(self):
         X = self.data.drop('revenue', axis=1)
         y = self.data['revenue']
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size,
-                                                                                random_state=random_state)
 
-    def train_model(self):
-        # Example: Training a Random Forest Classifier
-        model = RandomForestClassifier(random_state=42)
+        # Identify categorical columns
+        categorical_cols = X.select_dtypes(include=['object', 'category']).columns
+
+        # Apply one-hot encoding
+        self.preprocessor = ColumnTransformer(
+            transformers=[
+                ('onehot', OneHotEncoder(handle_unknown='ignore'), categorical_cols)
+            ], remainder='passthrough')
+
+        X_processed = self.preprocessor.fit_transform(X)
+
+        # Split the data
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X_processed, y, test_size=0.20,
+                                                                                random_state=42)
+
+    def feature_selection_based_on_importance(self, model, threshold=0.01):
+        if hasattr(model, 'feature_importances_'):
+            importances = model.feature_importances_
+
+            # Get feature names
+            feature_names = self.preprocessor.get_feature_names_out()
+            selected_features = [feature for feature, importance in zip(feature_names, importances) if
+                                 importance >= threshold]
+
+            # Filter the data
+            self.X_train = self.X_train[:, model.feature_importances_ >= threshold]
+            self.X_test = self.X_test[:, model.feature_importances_ >= threshold]
+
+            print("Selected features based on importance:", selected_features)
+            return self.X_train, self.X_test, selected_features
+        else:
+            print("Feature importance not available for this model type.")
+            return self.X_train, self.X_test, []
+
+    def train_linear_regression(self):
+        model = LinearRegression()
         model.fit(self.X_train, self.y_train)
         return model
 
+    def train_decision_tree(self):
+        model = DecisionTreeRegressor(random_state=42)
+        model.fit(self.X_train, self.y_train)
+        return model
+
+    def train_random_forest(self):
+        model = RandomForestRegressor(random_state=42)
+        model.fit(self.X_train, self.y_train)
+        return model
+
+    def train_xgboost(self):
+        model = XGBRegressor(random_state=42)
+        model.fit(self.X_train, self.y_train)
+        return model
+
+    def get_feature_importance(self, model):
+        if hasattr(model, 'feature_importances_'):
+            importances = model.feature_importances_
+            feature_names = self.preprocessor.get_feature_names_out()
+            # Create a Series with feature names and their importance scores
+            feature_importances = pd.Series(importances, index=feature_names)
+            return feature_importances.sort_values(ascending=False)
+        else:
+            return "Feature importance not available for this model type."
+
     def evaluate_model(self, model):
         predictions = model.predict(self.X_test)
-        accuracy = accuracy_score(self.y_test, predictions)
-        return accuracy
+
+        # Regression metrics
+        mse = mean_squared_error(self.y_test, predictions)
+        mae = mean_absolute_error(self.y_test, predictions)
+        rmse = np.sqrt(mse)
+        r2 = r2_score(self.y_test, predictions)
+
+        return {"MSE": mse, "MAE": mae, "RMSE": rmse, "R^2": r2}
